618c92dc2146c9e77fc20a968c8484756934e24f
bob <bob@bob-HP-ProBook-4440s>
2016-02-21 20:48:04 +0100

    Remote latex compiling done

--- www/js/constants.js
+++ www/js/constants.js
@@ -3,7 +3,8 @@ angular.module('starter')
 
 .constant('SERVER', {
     //url: 'http://your-server-url/ionic/topic'
-    url: 'http://localhost:8100/api'
+    url: 'http://localhost/collatexserver/api'
+    //url: 'http://localhost:8100/api'
 	//	http://localhost/development/public_html/api/restaurant/get.php?restaurant_id=3
 })
 
